7e37b6fb89642346e8f54d9aa165f85baa9ef14b
ABHINAV@0122 <131744556+abhi012211@users.noreply.github.com>
2024-04-16 23:27:43 +0530

    Add One Row to Tree.java
    java

---
+++ Add One Row to Tree.java
@@ -0,0 +1,24 @@
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(root==null){
+            return root;
+        }
+        if(depth==1){
+            TreeNode temp=new TreeNode(val);
+            temp.left=root;
+            root=temp;
+        }
+        if(depth-1==1){
+            TreeNode temp=new TreeNode(val);
+            temp.left=root.left;
+            root.left=temp;
+            TreeNode temp1=new TreeNode(val);
+            temp1.right=root.right;
+            root.right=temp1;
+        }
+        addOneRow(root.left,val,depth-1);
+        addOneRow(root.right,val,depth-1);
+        return root;
+
+    }
+}
